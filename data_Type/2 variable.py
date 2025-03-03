ac0c2481de9051ad3895c072bdad6d2c1bf1bfc9
lernermandy-07 <jadhavmandy9070@gmail.com>
2025-03-03 12:48:38 +0530

    Add files via upload

---
+++ data_Type/2 variable.py
@@ -0,0 +1,200 @@
+# variable in python 
+
+# in python no need for decleration
+
+# in python variable _= value for eg
+
+m= 16
+
+a = 23
+
+s = 33
+
+print(a + m + s)
+
+print(type(a))
+
+mandy = 7.7
+
+sandy = 38
+
+amy = 89
+
+print(mandy + sandy + amy ,type(mandy))
+
+mandy = 77
+
+print(mandy + sandy , type(mandy))
+
+d = 45
+
+m = 577
+
+print(d*m+m)
+
+maharashtra = 'highest gdp in the india '
+
+print(maharashtra,type(maharashtra))
+
+india = 'there are seven states in south india'
+
+print(india,type(india))
+
+education= ''''bca ,ba , 
+
+ma
+that is my educati qualification'''
+
+print(education,type(education))
+
+india = 29
+
+print(india,type(india))
+
+# in this lecture we learn that the rule of variable but ends with digit
+
+# 1- its never stars with digit 
+
+# 2 never mension keyword as a vairible
+
+# 3 the variable only allowed in __
+
+# 4 no length limit 
+
+# variable are case sensitive
+
+# there is no space valid in variable
+
+b = 39
+
+c =78
+
+d = 38
+
+print(type(d),b+c*d/4)
+
+m='''wo tajgi hai 
+jisse full bhi jle
+ chamak se chamdani dhale'''
+
+h = "mai sab kuch sikhunga aur data scientist banunga jarur "
+
+j =" mai mera spna jrur pura krunga "
+
+n = "i want to become a scientist "
+
+s = "the mandar jadhav is a data scientist "
+
+p = " the sacrifise is all worth it "
+
+print(h,j,n,s,p)
+
+b = 89
+
+print(b)
+
+v = ("bol chudiya bol kangafa ")
+
+print("v")
+
+p = float(b)
+
+print(p,type(p))
+
+q = bool(p)
+
+print(q,type(q))
+
+r = complex(q,b)
+
+print(r,type(r))
+
+a = 23
+
+print(a)
+
+b = 45
+
+print(b)
+
+c = 478
+
+print(c)
+
+d = ('mandar jadhav starting a new jorney with full of confidence and practice')
+
+print(d)
+
+e = ('elephant ')
+
+print(e)
+
+f = ('calory waith loss')
+
+print(f)
+
+j = 90
+
+print(j)
+
+print(a*j+c-b/c*(a*b))
+
+x = (67)
+
+print(x)
+
+print(j*x//b)
+
+z = ('zebra')
+
+print(z)
+
+bbb=99883843847
+
+print(bbb)
+
+a = 78
+
+n = 477
+
+h = 57
+
+print(a*b*c)
+
+
+variable = 34
+print(variable)
+
+mandy= 'golu'
+print(mandy)
+
+cgl='aso in mea'
+print(cgl)
+
+
+print(mandy + cgl)
+
+laptop='keyboard mouse buttons charger phone'
+print(laptop)
+
+my_bio='Mandar Rajendra Jadhav '
+
+graduation='YMIM Karad'
+
+skills='data science'
+
+print(my_bio+graduation+skills)
+
+
+a=12
+
+print(a)
+
+my_bio=('mandar')
+
+wekend123=(1,23,34,45)
+
+print(wekend123)
+
+
+
---
+++ data_Type/3 datatype.py
@@ -0,0 +1,412 @@
+# todays session about the data types of python
+
+# # none
+# numeric (float,int,complex,bool)
+# set
+# list
+# tuple
+# dictionary
+# String
+# range
+
+# none
+
+b = None
+
+print(b,type(b))
+
+d = None
+
+print(b,type(d))
+
+# int
+
+b = 56
+
+print(b,type(b))
+
+# float
+
+u = 677.90
+
+h = 90e1
+
+print(h,type(h))
+
+f4 = 2.3e4
+
+print(f4)
+
+
+
+print(h,type(h))
+
+# complex
+
+g = 6 + 9j
+
+print(g,type(g))
+
+r = 5 +78j
+
+print(r,type(r))
+
+print(g+r)
+
+# boolean
+
+p = b>h
+
+print(p,type(p))
+
+# list
+
+b = [67,89,325,277,63738,3778]
+
+print(b,type(b))
+
+# set
+
+n = {6373,7383,74784,78443,828,748}
+
+print(n,type(n))
+
+# tuple
+
+j = (688,7676,764,667,777)
+
+print(j,type(j))
+
+# string
+
+v = 'kiska hai ye tumko intajr mai na '
+
+print(v,type(v))
+
+# range
+
+m = range(1,10)
+
+print(m,type(m))
+
+c = list(range(10))
+
+print(c)
+
+# dictinary
+
+s ={'mandar':'samsung','swapnil':'oneplus','amar':'nokia'}
+
+print(s,type(s))
+
+# string
+
+my_bio='ny name is mandy'
+
+print(my_bio)
+
+my_bio1='''my name is mandy 
+mu father name is rajendra
+my mother name is sangita
+my education qualification is bca'''
+
+print(my_bio1)
+
+# string concatenation
+
+my_schoool='ssmvs'
+
+my_college='apkm'
+
+print(my_college+my_schoool)
+
+my_info='mandy'
+
+my_life='akshara'
+
+my_bio1='prasad'
+
+my_age='22'
+
+print(my_age+" "+my_bio+ " "+my_life+" "+my_bio1+" "+my_info)
+
+# second type to print
+
+print(my_info,my_age,my_bio,my_bio1,my_life)
+
+# index
+
+my_song='mere sapno ki rani kb ayegi tu'
+
+print(my_song)
+
+print(my_song[11])
+
+print(len(my_song))
+
+print(my_song[0:20])
+
+print(my_song[0:37])
+
+for i in my_song:
+    print(i)
+
+print(my_song[-32:78])
+
+print(my_song[5:])
+
+print(my_song[:78])
+
+print(my_song[7:35:3])
+
+# predefined function in python
+
+# capitalise
+
+# lower()
+
+# upper()
+
+# join()
+
+# split()
+
+# isalpha()
+
+# isupper()
+
+# islower()
+
+# Find ()
+
+# capitalise()
+
+# title()
+
+# isalnum()
+
+# isdigit()
+
+# typecasting in python 
+
+variable=78
+
+b=float(variable)
+
+print(b,type(b))
+
+
+
+
+m='mamdy'
+print(m[2])
+
+alphabet=('aabacdefghij k l m n o p q r s t u v w x y z')
+
+print(alphabet,len(alphabet))
+
+print(alphabet[-5:])
+
+print(alphabet[2:-1])
+
+print(alphabet[2:9:3])
+
+print(alphabet[1:10:2])
+
+print(alphabet[-7:1])
+
+for i in alphabet:
+    print(i)
+
+
+# data types in python for practice
+
+# int
+
+number=23
+
+print(number,type(number)) 
+
+# string
+
+song='kdhi tu oslatya dhara '
+
+print(song,type(song))
+
+# booolean
+
+b=True
+
+print(b,type(b))
+
+# float
+
+f=34.56
+
+print(f,type(f))
+
+# index in python 
+
+# in this index the no will the position of the text for eg
+
+song='nave pankh pasara jivnache git gare gund gare'
+
+print(song[1:12])
+
+print(len(song))
+
+print(song[0:18])
+
+print(song[-8:])
+
+print(song[0:7])
+
+print(song[5:12])
+
+print(song[-45:])
+
+print(song[0:10])
+
+# type casting python in the type 
+
+# type means casting we convertwed data type into another data type
+
+# int to other
+
+a=23
+
+print(a,type(a))
+
+b=float(a)
+
+print(b,type(b))
+
+c=str(a)
+
+print(c,type(c))
+
+d=bool(a)
+
+print(d,type(d))
+
+f=complex(a)
+
+print(f,type(f))
+
+
+# str to other
+
+s='34'
+
+print(s,type(s))
+
+c=int(s)
+
+print(type,(c))
+
+d=float(s)
+
+print(s,type(s))
+
+d=bool(s)
+
+print(d,type(d))
+
+g=complex(s)
+
+print(g,type(g))
+
+
+# float to other
+
+f=34.56
+
+print(f,type(f))
+
+a=int(f)
+
+print(f,type(a))
+
+z=str(f)
+
+print(z,type(z))
+
+x=complex(f)
+
+print(x,type(x))
+
+c=bool(f)
+
+print(c,type(c))
+
+
+
+# bool to other
+
+b=True
+
+print(b,type(b))
+
+c=int(b)
+
+print(c,type(c))
+
+v=str(b)
+
+print(v,type(v))
+
+n=complex(b)
+
+print(b,type(n))
+
+m=float(b)
+
+print(m,type(m))
+
+
+# complex to other
+
+l=23+23j
+
+print(l,type(l))
+
+# k=int(l)
+
+# print(k,type(k))
+
+# j=float(l)
+
+# print(j,type(j))
+
+h=str(l)
+
+print(h,type(h))
+
+g=bool(l)
+
+print(g,type(g))
+
+
+
+a=(5)
+
+print(a,type(a))
+
+b=('data scientist')
+
+print(b,type(b))
+
+c=(34.07)
+
+print(c,type(c))
+
+d=(True)
+
+print(d,type(d))
+
+e=(7+7j)
+
+print(e,type(e))
+
+
+
